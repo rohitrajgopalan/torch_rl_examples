cbd56820ec3a1fb757bf07c748c5913d54b61d42
Rohit Gopalan <rohitgopalan@hotmail.com>
2021-06-16 08:07:17 +0930

    Delete acrobot.py

--- acrobot.py
+++
@@ -1,8 +0,0 @@
-import gym
-try:
-    from common import run_all_discrete_methods
-except ImportError:
-    from .common import run_all_discrete_methods
-
-env = gym.make('Acrobot-v1')
-run_all_discrete_methods(env, 'acrobot')
