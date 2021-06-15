468c67da4a9e683fc7109614bb693905d750ab7a
Rohit Gopalan <rohitgopalan@hotmail.com>
2021-06-16 08:07:48 +0930

    Delete pendulum.py

--- pendulum.py
+++
@@ -1,8 +0,0 @@
-import gym
-try:
-    from common import run_actor_critic_continuous_methods
-except ImportError:
-    from .common import run_actor_critic_continuous_methods
-
-env = gym.make('Pendulum-v0')
-run_actor_critic_continuous_methods(env, 'pendulum')
