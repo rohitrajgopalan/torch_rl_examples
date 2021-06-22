8be2b620d54bda87b4e84ccb3ab0060e9a1e7b18
Rohit Gopalan <rohitgopalan@hotmail.com>
2021-06-22 12:28:27 +0930

    Add files via upload

--- openai.py
+++ openai.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+
 try:
     from common import run_all_discrete_methods, run_actor_critic_continuous_methods
 except ImportError:
@@ -29,17 +30,17 @@ run_all_discrete_methods(env_discrete, 'lunar_lander', 100)
 env_discrete = gym.make('MountainCar-v0')
 run_all_discrete_methods(env_discrete, 'mountain_car', 0, np.array([0.5, 0.5]))
 
-env_continuous = gym.make('LunarLanderContinuous-v2')
-run_actor_critic_continuous_methods(env_continuous, 'lunar_lander')
-
-env_continuous = gym.make('MountainCarContinuous-v0')
-run_actor_critic_continuous_methods(env_continuous, 'mountain_car')
-
-env = gym.make('Pendulum-v0')
-run_actor_critic_continuous_methods(env, 'pendulum')
-
-env = gym.make('BipedalWalker-v3')
-run_actor_critic_continuous_methods(env, 'bipedal_walker')
-
-env_hardcore = gym.make('BipdealWalkerHardcore-v3')
-run_actor_critic_continuous_methods(env_hardcore, 'bipedal_walker_hardcore')
+# env_continuous = gym.make('LunarLanderContinuous-v2')
+# run_actor_critic_continuous_methods(env_continuous, 'lunar_lander')
+#
+# env_continuous = gym.make('MountainCarContinuous-v0')
+# run_actor_critic_continuous_methods(env_continuous, 'mountain_car')
+#
+# env = gym.make('Pendulum-v0')
+# run_actor_critic_continuous_methods(env, 'pendulum')
+#
+# env = gym.make('BipedalWalker-v3')
+# run_actor_critic_continuous_methods(env, 'bipedal_walker')
+#
+# env_hardcore = gym.make('BipdealWalkerHardcore-v3')
+# run_actor_critic_continuous_methods(env_hardcore, 'bipedal_walker_hardcore')
